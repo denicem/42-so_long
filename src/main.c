f538f5403b024fc8bc6dc649309082bdd13a018e
Denice Montemayor <dmontema@1-D-13.42heilbronn.de>
2022-01-20 17:20:24 +0100

    player can move around and will not move through a wall - only in terminal yet.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 20:05:53 by dmontema          #+#    #+#             */
-/*   Updated: 2022/01/19 23:13:15 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/01/20 17:14:38 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,37 @@ void	find_player_pos(t_data *data)
 	}
 }
 
+void	move_player(t_data *data, int keycode)
+{
+	t_vector old_pos;
+	
+	old_pos = data->player_pos;
+	if (keycode == 0)
+		(data->player_pos.x)--;
+	if (keycode == 1)
+		(data->player_pos.y)++;
+	if (keycode == 2)
+		(data->player_pos.x)++;
+	if (keycode == 13)
+		(data->player_pos.y)--;
+	if (data->map[data->player_pos.y][data->player_pos.x] == '1')
+	{
+		data->player_pos.x = old_pos.x;
+		data->player_pos.y = old_pos.y;
+	}
+	else
+	{
+		data->map[old_pos.y][old_pos.x] = '0';
+		data->map[data->player_pos.y][data->player_pos.x] = 'P';
+	}
+	// mlx_clear_window(data->mlx, data->mlx_win);
+	// draw_map(data);
+	// mlx_destroy_image(data->mlx, data->textures[player].img);
+	// mlx_put_image_to_window(data->mlx, data->mlx_win, data->textures[floor].img, old_pos.x * TXT_PX, old_pos.y * TXT_PX);
+	// mlx_put_image_to_window(data->mlx, data->mlx_win, data->textures[player].img, data->player_pos.x * TXT_PX, data->player_pos.y * TXT_PX);
+	print_map(data);
+}
+
 void	quit_game(t_data *data)
 {
 	free(data->map);
@@ -51,7 +82,8 @@ int key_hook(int keycode, t_data *data)
 	if (keycode == 53)
 		quit_game(data);
 	if (keycode == 0 || keycode == 1 || keycode == 2 || keycode == 13)
-		printf("moves: %d\n", ++(data->moves));
+		move_player(data, keycode);
+		// printf("moves: %d\n", ++(data->moves));
 	return (1);
 }
 
@@ -66,6 +98,7 @@ int	main(int argc, char *argv[])
 		data.mlx = mlx_init();
 		data.mlx_win = mlx_new_window(data.mlx, data.width * TXT_PX, data.height * TXT_PX, "so_long");
 		set_textures(&data);
+		printf("%d %d\n", data.textures[0].size.x, data.textures[0].size.y);
 		draw_map(&data);
 		data.moves = 0;
 		find_player_pos(&data);
